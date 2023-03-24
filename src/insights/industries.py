da2fd6a9f804dcb1643164e6596c07d9224affa7
Ewerton Ferreira <ewertonferreira@msn.com>
2023-03-24 16:19:30 -0300

    Req 07

--- src/insights/industries.py
+++ src/insights/industries.py
@@ -9,24 +9,10 @@ def get_unique_industries(path: str) -> List[str]:
         if row["industry"] != '':
             industries_list.add(row['industry'])
     return list(industries_list)
-    """Checks all different industries and returns a list of them
-
-    Must call `read`
-
-    Parameters
-    ----------
-    path : str
-        Must be passed to `read`
-
-    Returns
-    -------
-    list
-        List of unique industries
-    """
-    raise NotImplementedError
 
 
 def filter_by_industry(jobs: List[Dict], industry: str) -> List[Dict]:
+    return [job for job in jobs if job['industry'] == industry]
     """Filters a list of jobs by industry
 
     Parameters
