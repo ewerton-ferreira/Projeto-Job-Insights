010e5b2a3e413e2f08ac26fb85abbb9749903988
Ewerton Ferreira <ewertonferreira@msn.com>
2023-03-24 16:12:42 -0300

    Req 03

--- src/insights/industries.py
+++ src/insights/industries.py
@@ -1,7 +1,14 @@
 from typing import List, Dict
+from src.insights.jobs import read
 
 
 def get_unique_industries(path: str) -> List[str]:
+    file = read(path)
+    industries_list = set()
+    for row in file:
+        if row["industry"] != '':
+            industries_list.add(row['industry'])
+    return list(industries_list)
     """Checks all different industries and returns a list of them
 
     Must call `read`
