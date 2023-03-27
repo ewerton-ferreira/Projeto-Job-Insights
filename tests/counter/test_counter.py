985ed0f82ce0da7ef710f9da0c9aa7e231524492
Ewerton Ferreira <ewertonferreira@msn.com>
2023-03-27 17:26:06 -0300

    Req 10 - segunda busca

--- tests/counter/test_counter.py
+++ tests/counter/test_counter.py
@@ -2,5 +2,8 @@ from src.pre_built.counter import count_ocurrences
 
 
 def test_counter():
-    result_counter = count_ocurrences(('data/jobs.csv'), 'python')
-    assert result_counter == 1639
+    result_counter_word1 = count_ocurrences(('data/jobs.csv'), 'python')
+    assert result_counter_word1 == 1639
+
+    result_counter_word2 = count_ocurrences(('data/jobs.csv'), 'Javascript')
+    assert result_counter_word2 == 122
