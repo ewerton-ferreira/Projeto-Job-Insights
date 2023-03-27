f0d1b421d7de6bf8dd3969ebbab6be5b5d01893b
Ewerton Ferreira <ewertonferreira@msn.com>
2023-03-27 16:39:41 -0300

    Req 11

--- tests/brazilian/test_brazilian_jobs.py
+++ tests/brazilian/test_brazilian_jobs.py
@@ -1,5 +1,14 @@
-# from src.pre_built.brazilian_jobs import read_brazilian_file
+from src.pre_built.brazilian_jobs import read_brazilian_file
 
 
 def test_brazilian_jobs():
-    pass
+    result_jobs = read_brazilian_file('tests/mocks/brazilians_jobs.csv')
+
+    for job in result_jobs:
+        # print(job)
+        for key, value in job.items():
+            assert key in [
+                'title', 'salary', 'type']
+            assert isinstance(value, str)
+        # print(key)
+        # print(value)
