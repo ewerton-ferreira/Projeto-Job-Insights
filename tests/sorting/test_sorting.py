b65668f6d7c3a9cc9dccb143940553ac4731638a
Ewerton Ferreira <ewertonferreira@msn.com>
2023-03-27 17:41:17 -0300

    Req 12

--- tests/sorting/test_sorting.py
+++ tests/sorting/test_sorting.py
@@ -1,5 +1,19 @@
-# from src.pre_built.sorting import sort_by
+from src.pre_built.sorting import sort_by
+from src.insights.jobs import read
 
 
 def test_sort_by_criteria():
-    pass
+    file = read("data/jobs.csv")
+    result = [row for row in file]
+
+    mock_min_salary = [result[1], result[2]]
+    sort_by(mock_min_salary, 'min_salary')
+    assert mock_min_salary == [result[1], result[2]]
+
+    mock_max_salary = [result[1], result[2]]
+    sort_by(mock_max_salary, 'max_salary')
+    assert mock_max_salary == [result[1], result[2]]
+
+    mock_date_posted = [result[1], result[2]]
+    sort_by(mock_date_posted, 'date_posted')
+    assert mock_date_posted == [result[2], result[1]]
