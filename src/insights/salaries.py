cca5303df26de1b4108c805950ae0ea93006b2e2
Ewerton Ferreira <ewertonferreira@msn.com>
2023-03-27 16:15:23 -0300

    Req 09

--- src/insights/salaries.py
+++ src/insights/salaries.py
@@ -50,18 +50,11 @@ def filter_by_salary_range(
     jobs: List[dict],
     salary: Union[str, int]
 ) -> List[Dict]:
-    """Filters a list of jobs by salary range
-
-    Parameters
-    ----------
-    jobs : list
-        The jobs to be filtered
-    salary : int
-        The salary to be used as filter
-
-    Returns
-    -------
-    list
-        Jobs whose salary range contains `salary`
-    """
-    raise NotImplementedError
+    filter_salary = []
+    for job in jobs:
+        try:
+            if matches_salary_range(job, salary):
+                filter_salary.append(job)
+        except ValueError:
+            ('Invalid salary range')
+    return filter_salary
